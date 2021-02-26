f021a989d79f191416e7e9b743fee42e62f8188f
Loïc Hermann <loic.hermann@sciam.fr>
2021-02-26 12:02:22 +0100

    render react component & mock router in tests

--- src/test/js/test.test.tsx
+++ src/test/js/test.test.tsx
@@ -1,6 +1,22 @@
-import api from 'api';
+import {StackContextProvider} from 'contexts/StackContext';
+import {DataContextProvider} from 'contexts/DataContext';
+import {render, mount, cleanup} from '@testing-library/react';
+import { useRouter } from 'next/router';
+
+jest.mock("next/router");
 
 test('test if jest is working', () => {
-    console.log(api)
+
+    useRouter.mockReturnValue({
+        query: 
+            {stackId: undefined, 
+            serviceId: undefined, 
+            networkId: undefined}
+    });
+    render(
+    <DataContextProvider>
+        <StackContextProvider>
+        </StackContextProvider>
+    </DataContextProvider>);
     expect(3).toBe(3);
 });
